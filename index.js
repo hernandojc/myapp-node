2e724e9e8d7bfcc8fb5bb0d1ccc5baa72b92d120
Hernando Junior Julio Castillo <hernando.castillo@chiper.co>
2024-04-16 17:25:43 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const puerto = 3000
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello world')
+})
+
+app.listen(puerto, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en el puerto:', 3000);
+});
